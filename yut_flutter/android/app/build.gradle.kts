373eb1c73107a44ad7de2f8ddb7bd574e94b1680
Jeff Chan <superjeffc@users.noreply.github.com>
2026-07-24 01:22:17 +0000

    Target Android 16 (API Level 36) in build.gradle.kts

--- yut_flutter/android/app/build.gradle.kts
+++ yut_flutter/android/app/build.gradle.kts
@@ -6,7 +6,7 @@ plugins {
 
 android {
     namespace = "com.jeffreychan.yunnori"
-    compileSdk = flutter.compileSdkVersion
+    compileSdk = 36
     ndkVersion = flutter.ndkVersion
 
     compileOptions {
@@ -16,10 +16,8 @@ android {
 
     defaultConfig {
         applicationId = "com.jeffreychan.yunnori"
-        // You can update the following values to match your application needs.
-        // For more information, see: https://flutter.dev/to/review-gradle-config.
         minSdk = flutter.minSdkVersion
-        targetSdk = flutter.targetSdkVersion
+        targetSdk = 36
         versionCode = flutter.versionCode
         versionName = flutter.versionName
     }
